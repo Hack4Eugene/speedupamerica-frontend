d5c749e2e82c6f9edf61cf5b4a194a2462700adc
Noah Brenner <noah@noahbrenner.com>
2019-07-01 14:35:31 -0700

    Add .editorconfig file and normalize indentation

--- test/template.spec.ts
+++ test/template.spec.ts
@@ -1,27 +1,27 @@
 const assert = require('chai').assert;
 
 describe('Test Class Block', () =>  {
-    before(() => {
-        // runs before all tests in this block
+  before(() => {
+    // runs before all tests in this block
+  });
+
+  after(() => {
+    // runs after all tests in this block
+  })
+
+  beforeEach(() => {
+    // runs before each test in this block
+  });
+
+  afterEach(() => {
+    // runs after each test in this block
+  });
+
+  describe('Test 1 #get_all', () => {
+    it('Should get all', (done) => {
+      assert(true);
+
+      done();
     });
-
-    after(() => {
-        // runs after all tests in this block
-    })
-
-    beforeEach(() => {
-        // runs before each test in this block
-    });
-
-    afterEach(() => {
-        // runs after each test in this block
-    });
-
-    describe('Test 1 #get_all', () => {
-        it('Should get all', (done) => {
-            assert(true);
-
-            done();
-        });
-    });
+  });
 });
