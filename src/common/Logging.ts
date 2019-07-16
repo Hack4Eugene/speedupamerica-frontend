673cd7eb30a30675079f8e0d1ccd9c1254bcd180
Kourchenko <dkourchenko@gmail.com>
2019-07-15 20:45:04 -0700

    Liiiiiiiiiiiiiiiiiiinter errrrr

--- src/common/Logging.ts
+++ src/common/Logging.ts
@@ -7,33 +7,33 @@ export class Logging {
     private level: string = getEnv() === 'production' ? 'info' : 'debug';
 
     constructor() {
-        this.logger = winston.createLogger({
-    	    level: this.level,
-    		format: winston.format.combine(
-    			winston.format.label({label: getEnv()}),
-    			winston.format.timestamp(),
-    			winston.format.json(),
-    			winston.format.printf(() => {
-    				return `ENV: ${getEnv()}`;
-    			}),
-    			winston.format.printf(() => {
-    				return 'type: application';
-    			}),
-    		),
-    		defaultMeta: {service: 'speedupamerica-frontend'},
-    		transports: [
-    			new winston.transports.Console({
-    				format: winston.format.simple(),
-    				stderrLevels: ['error'],
-    				consoleWarnLevels: ['warn', 'debug', 'info'],
-    			}),
-    			new Sentry({
-    				dsn: 'https://34760911653f40a18ba07c39a6a37002@sentry.io/1488146',
-    				level: 'info',
-    			}),
-    		],
-    	});
-    }
+      this.logger = winston.createLogger({
+            level: this.level,
+            format: winston.format.combine(
+                winston.format.label({label: getEnv()}),
+                winston.format.timestamp(),
+                winston.format.json(),
+                winston.format.printf(() => {
+                  return `ENV: ${getEnv()}`;
+                }),
+                winston.format.printf(() => {
+                  return 'type: application';
+                }),
+            ),
+defaultMeta: {service: 'speedupamerica-frontend'},
+transports: [
+    new winston.transports.Console({
+        format: winston.format.simple(),
+        stderrLevels: ['error'],
+        consoleWarnLevels: ['warn', 'debug', 'info'],
+    }),
+    new Sentry({
+        dsn: 'https://34760911653f40a18ba07c39a6a37002@sentry.io/1488146',
+        level: 'info',
+    }),
+],
+});
+}
 
     log(msg: string, additionalInfo: any[]) {
     	this.logger.log(this.level, msg, additionalInfo);
