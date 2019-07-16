2c7e42209db4b2685172d12a08c06ff886ea573f
Kourchenko <dkourchenko@gmail.com>
2019-07-15 20:18:28 -0700

    Organized folders for logging; added logging to main.ts

--- src/common/Logging.ts
+++ src/common/Logging.ts
@@ -1,30 +1,38 @@
 import * as winston from 'winston';
 import { getEnv } from '../helper/config';
+import { Format } from 'logform';
+const Sentry = require('winston-raven-sentry')
+
+const format: Format = winston.format.combine(
+    winston.format.label({ label:  getEnv()}),
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.printf(() => {
+        return `ENV: ${getEnv()}`;
+    }),
+    winston.format.printf(() => {
+        return 'type: application';
+    })
+);
 
 export class Logging {
     private logger: winston.Logger;
-    private level: string = getEnv() === 'production' ? 'debug' : 'info';
+    private level: string = getEnv() === 'production' ? 'info' : 'debug';
 
-    private constructor() {
+    constructor() {
         this.logger = winston.createLogger({
             level: this.level,
+            format: format,
             defaultMeta: { service: 'speedupamerica-frontend'},
-            format: winston.format.combine(
-                winston.format.label({ label:  getEnv()}),
-                winston.format.timestamp(),
-                winston.format.json(),
-                winston.format.printf(() => {
-                    return `ENV: ${getEnv()}`;
-                }),
-                winston.format.printf(() => {
-                    return 'type: application';
-                })
-            ),
             transports: [
                 new winston.transports.Console({
                     format: winston.format.simple(),
                     stderrLevels: ['error'],
                     consoleWarnLevels: ['warn', 'debug', 'info']
+                }),
+                new Sentry({
+                    dsn: 'https://34760911653f40a18ba07c39a6a37002@sentry.io/1488146',
+                    level: 'info'
                 })
             ]
         });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import {testConnection} from './dal/test';
+import { Logging } from '../src/common/Logging';
 
 // Temp, just here to demonstrate connection
 // Delete when we setup Express or whatever router we use
 testConnection();
+
+// Delete when we use Logging for requests.
+let logging: Logging = new Logging();
+logging.log('Starting', []);
+logging.log('Stopping', []);
