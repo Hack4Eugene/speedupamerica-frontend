7175404b39e50e8160a22a7035ef1b31c0f09cf0
Kourchenko <dkourchenko@gmail.com>
2019-07-16 22:20:09 -0700

    Cleanup logging for improved logs; cleanup env variables

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,12 +1,16 @@
 
-function getEnvironmentVariables() {
-  const env: string = process.env['NODE_ENV'] || 'local';
-  const logglyToken: string | undefined = process.env['LOGGLY_TOKEN'];
+function environmentVariables() {
+  const NODE_ENV: string = process.env['NODE_ENV'] || 'local';
+  const DB_HOSTNAME: string = process.env['NODE_ENV'] || 'localhost';
+  const LOGGLY_TOKEN: string | undefined = process.env['LOGGLY_TOKEN'];
+  const NPM_PACKAGE_VERSION: string = process.env['npm_package_version'] || '';
 
   return {
-    env,
-    logglyToken,
+    NODE_ENV,
+    DB_HOSTNAME,
+    LOGGLY_TOKEN,
+    NPM_PACKAGE_VERSION
   };
 }
 
-export {getEnvironmentVariables};
+export {environmentVariables};
--- src/common/logging.ts
+++ src/common/logging.ts
@@ -1,53 +1,25 @@
 import {createLogger, format, transports} from 'winston';
-const {Loggly} = require('winston-loggly-bulk');
-import {getEnvironmentVariables} from './config';
+import {environmentVariables} from './config';
 import winston = require('winston');
+const {Loggly} = require('winston-loggly-bulk');
 
-/**
- * Winston Log Object
- * Send logs to Console if message level <= 'error'.
- * Send logs to Sentry if message level is at least 'info'.
- */
-const envVariables = getEnvironmentVariables();
-const NODE_ENV = envVariables.env;
+const {
+  NODE_ENV,
+  LOGGLY_TOKEN,
+  DB_HOSTNAME,
+  NPM_PACKAGE_VERSION
+} = environmentVariables();
+
+// Winston Logger
+// Send message to STDOUT (console) if in development env.
 const logging =
   createLogger({
-    level: envVariables.env === 'production' ? 'error' : 'debug',
+    level: NODE_ENV === 'development' ? 'debug' : 'info',
     defaultMeta: {service: 'speedupamerica-frontend'},
     exitOnError: false,
     format: format.combine(
-        format.timestamp(),
-        format.json(),
-        format.printf(() => {
-          return `ENV: ${NODE_ENV}`;
-        }),
-        format.printf(() => {
-          return 'type: application';
-        }),
-    ),
-    transports: [
-      new transports.Console({
-        level: 'debug',
-        format: format.simple(),
-        stderrLevels: ['error'],
-        consoleWarnLevels: ['warn', 'debug', 'info'],
-      }),
-    ],
-  });
-
-if (!envVariables.logglyToken) {
-  winston.add(new Loggly({
-    level: 'debug',
-    token: envVariables.logglyToken,
-    tags: ['NodeJS'],
-    subdomain: `https://logs-01.loggly.com/inputs/${envVariables.logglyToken}/tag/speedupamerica-v2`,
-    json: true,
-    format: format.combine(
         format.printf(({level, timestamp, message}) => {
           return `summary: ${level} ${timestamp} ${NODE_ENV}: ${message}`;
-        }),
-        format.printf(({level}) => {
-          return `severity: ${level}`;
         }),
         format.timestamp(),
         format.json(),
@@ -57,6 +29,54 @@ if (!envVariables.logglyToken) {
         format.printf(() => {
           return 'type: application/json';
         }),
+        format.printf(({level}) => {
+          return `severity: ${level}`;
+        }),
+    ),
+    transports: [
+      new transports.Console({
+        level: 'debug',
+        format: format.combine(
+          format.colorize(),
+          format.printf(({level, timestamp, message}) => {
+            return `summary: ${level} ${timestamp} ${NODE_ENV}: ${message}`;
+          }),
+        ),
+        stderrLevels: ['error', 'debug'],
+        consoleWarnLevels: ['warn', 'debug'],
+      }),
+    ],
+  });
+
+// Configure Loggly if valid LOGGLY_TOKEN
+if (!LOGGLY_TOKEN) {
+  winston.add(new Loggly({
+    level: 'debug',
+    token: LOGGLY_TOKEN,
+    tags: ['NodeJS'],
+    subdomain: `https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/speedupamerica-v2`,
+    json: true,
+    format: format.combine(
+        format.printf(({level, timestamp, message}) => {
+          return `summary: ${level} ${timestamp} ${NODE_ENV}: ${message}`;
+        }),
+        format.timestamp(),
+        format.json(),
+        format.printf(() => {
+          return `env: ${NODE_ENV}`;
+        }),
+        format.printf(() => {
+          return 'type: application/json';
+        }),
+        format.printf(({level}) => {
+          return `severity: ${level}`;
+        }),
+        format.printf(() => {
+          return `hostname: ${DB_HOSTNAME}`;
+        }),
+        format.printf(() => {
+          return `packageVersion: ${NPM_PACKAGE_VERSION}`;
+        })
     ),
   }));
 }
