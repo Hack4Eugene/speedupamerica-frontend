856c6a8e57ff1a228e60e2cd93f28fed9b90ea0b
Ryan Olds <ryanrolds@gmail.com>
2019-07-20 14:04:53 -0700

    Fixed port

--- src/common/config.ts
+++ src/common/config.ts
@@ -6,7 +6,7 @@ function environmentVariables() {
   const HOSTNAME: string = process.env['HOSTNAME'] || os.hostname();
   const LOGGLY_TOKEN: string | undefined = process.env['LOGGLY_TOKEN'];
   const NPM_PACKAGE_VERSION: string = process.env['npm_package_version'] || '';
-  const PORT: number = parseInt(process.env['PORT'] || '8081', 10);
+  const PORT: number = parseInt(process.env['PORT'] || '8080', 10);
 
   return {
     NODE_ENV,
