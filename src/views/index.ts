a3775298611f9f41c962b7f1724dae257fbd2d39
Kourchenko <dkourchenko@gmail.com>
2019-07-22 14:59:30 -0700

    Lint fix

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,4 @@
 // Disallow webpage index,follow if in development
-document.URL.includes('test.speedupamercica.com') ? 
-  document.head.append('<meta name="robots" content="noindex,nofollow" />') 
+document.URL.includes('test.speedupamercica.com') ?
+  document.head.append('<meta name="robots" content="noindex,nofollow" />')
   : null;
