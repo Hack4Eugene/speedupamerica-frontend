052195be8a42908352388a7b416f22d31a642252
Kourchenko <dkourchenko@gmail.com>
2019-07-22 14:58:18 -0700

    Added robots meta tag if in development

---
+++ src/views/index.ts
@@ -0,0 +1,4 @@
+// Disallow webpage index,follow if in development
+document.URL.includes('test.speedupamercica.com') ? 
+  document.head.append('<meta name="robots" content="noindex,nofollow" />') 
+  : null;
