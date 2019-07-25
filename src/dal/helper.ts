e252d044f40c08315beb46bd2487d02688206dc5
Kourchenko <dkourchenko@gmail.com>
2019-07-25 11:24:36 -0700

    Log errs

--- src/dal/helper.ts
+++ src/dal/helper.ts
@@ -13,7 +13,7 @@ function verifySubmission(submission: Submission) {
   // No invalid coordinates
   if ((latitude < -90 || latitude > 90) ||
     (longitude < -180 || longitude > 180)) {
-    logging.error('Submission Create (latitude, longitude).');
+    logging.error('Submission Create (latitude, longitude).', latitude, longitude);
     throw errInvalidArgs;
   }
 
