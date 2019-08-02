4dd3c3faadf7faf9786fc4c64fb7693a03d1b59d
Kourchenko <dkourchenko@gmail.com>
2019-08-01 18:51:10 -0700

    Use os.hostname() as fallback for 'hostname'

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,6 +1,8 @@
+const os = require('os');
+
 function environmentVariables() {
   const NODE_ENV: string = process.env['NODE_ENV'] || 'local';
-  const HOSTNAME: string = process.env['HOSTNAME'] || '';
+  const HOSTNAME: string = process.env['HOSTNAME'] || os.hostname();
   const LOGGLY_TOKEN: string | undefined = process.env['LOGGLY_TOKEN'];
   const NPM_PACKAGE_VERSION: string = process.env['npm_package_version'] || '';
   const PORT: number = parseInt(process.env['PORT'] || '8080', 10);
