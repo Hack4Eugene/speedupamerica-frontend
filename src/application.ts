cb4262509bbbc628632157b273a4bed0d995ca08
Kourchenko <dkourchenko@gmail.com>
2019-08-07 22:38:49 -0700

    Templating; main, header, 404 pages

--- src/application.ts
+++ src/application.ts
@@ -1,5 +1,7 @@
+import path = require('path');
 import express = require('express');
 import {Response, Request, NextFunction} from 'express';
+import handlebars = require('express-handlebars');
 import * as HttpStatus from 'http-status-codes';
 
 import {accessLogMiddleware} from './common/access_log';
@@ -13,12 +15,22 @@ export const app = express();
 // We want remote IP from LB's X-Forward-For header
 app.enable('trust proxy');
 
+// Express Handlebars
+app.set('views', path.join(__dirname, '/views/layouts'));
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+}));
+app.set('view engine', 'handlebars');
+
 // Setup access logs
 app.use(accessLogMiddleware());
 
 // Routes
 app.get('/health', healthController.health);
 app.get('/', homeController.index);
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Test routes
 app.get('/test/error', testController.error);
@@ -31,11 +43,10 @@ app.use((req:Request, res:Response, _next:NextFunction) => {
 
   // Handle json
   if (req.accepts('json')) {
-    res.json({'status': 'error', 'error': 'not found'});
+    res.render('404', {'status': 'error', 'error': 'not found'});
     return;
   }
-
-  res.send('Not found');
+  res.render('404');
 });
 
 // Error handler
--- src/controller/home.ts
+++ src/controller/home.ts
@@ -2,7 +2,7 @@ import {Response, Request} from 'express';
 import * as HttpStatus from 'http-status-codes';
 
 function index(_req:Request, res:Response) {
-  res.status(HttpStatus.OK).end('home');
+  res.status(HttpStatus.OK).render('home');
 }
 
 export {index};
